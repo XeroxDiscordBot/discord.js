a52aa3c24e103c8f51094d34b81d0c672601a7cd
Jake Ward <geek@gaminggeek.dev>
2022-11-16 21:17:38 +0000

    types: add ForumChannel

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6040,7 +6040,8 @@ export type AnyChannel =
   | StoreChannel
   | TextChannel
   | ThreadChannel
-  | VoiceChannel;
+  | VoiceChannel
+  | ForumChannel;
 
 export type TextBasedChannel = Extract<AnyChannel, { messages: MessageManager }>;
 
@@ -6069,7 +6070,7 @@ export type VoiceBasedChannel = Extract<AnyChannel, { bitrate: number }>;
 
 export type GuildBasedChannel = Extract<AnyChannel, { guild: Guild }>;
 
-export type NonThreadGuildBasedChannel = Exclude<GuildBasedChannel, ThreadChannel>;
+export type NonThreadGuildBasedChannel = Exclude<GuildBasedChannel, ThreadChannel | ForumChannel>;
 
 export type GuildTextBasedChannel = Extract<GuildBasedChannel, TextBasedChannel>;
 
